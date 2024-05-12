6a0f1213cc02fd46e5e109474ca62205c6f38de9
Cyph3r-6214 <164779731+Cyph3r-6214@users.noreply.github.com>
2024-05-12 16:44:14 -0500

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,38 +2,53 @@
 function scrollToTop() {
   window.scrollTo({
     top: 0,
-    behavior: 'smooth'
+    behavior: "smooth",
   });
 }
 
 window.onscroll = function () {
   var scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-  var scrollToTopBtn = document.querySelector('.scroll-to-top');
-  if (scrollPos > 20) { // Adjusted from 100 to 20 pixels
-    scrollToTopBtn.style.display = 'block';
+  var scrollToTopBtn = document.querySelector(".scroll-to-top");
+  if (scrollPos > 20) {
+    // Adjusted from 100 to 20 pixels
+    scrollToTopBtn.style.display = "block";
   } else {
-    scrollToTopBtn.style.display = 'none';
+    scrollToTopBtn.style.display = "none";
   }
 };
 
 /*-- LOGIN REGISTER MODAL --*/
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   /*-- NAV REGISTER/LOGIN REDIRECT--*/
-  document.getElementById('loginRedirect').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent the default link behavior
-    $('#registerModal').modal('hide'); // Hide the register modal if it's open
-    $('#loginModal').modal('show'); // Show the login modal
-  });
+  document
+    .getElementById("loginRedirect")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      $("#registerModal").modal("hide"); // Hide the register modal if it's open
+      $("#loginModal").modal("show"); // Show the login modal
+    });
 
-  document.getElementById('registerRedirect').addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent the default link behavior
-    $('#loginModal').modal('hide'); // Hide the login modal if it's open
-    $('#registerModal').modal('show'); // Show the register modal
-  });
+  document
+    .getElementById("registerRedirect")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default link behavior
+      $("#loginModal").modal("hide"); // Hide the login modal if it's open
+      $("#registerModal").modal("show"); // Show the register modal
+    });
 });
 
 /*-- ABOUT TYPING ANIMATION FOR t3Rminal_cmd --*/
-const terminalCmdProfessions = ["Web Designer", "Web Developer", "USMC Veteran", "Photographer", "Runner", "Snowboarder", "Foodie", "Investigator", "Police Officer"];
+const terminalCmdProfessions = [
+  "Web Designer",
+  "Web Developer",
+  "USMC Veteran",
+  "Photographer",
+  "Runner",
+  "Snowboarder",
+  "Foodie",
+  "Investigator",
+  "Police Officer",
+];
 let terminalCmdIndex = 0;
 
 function terminalCmdTypeWriter() {
@@ -43,7 +58,8 @@ function terminalCmdTypeWriter() {
 
   function innerTypeWriter() {
     if (charIndex < currentProfession.length) {
-      document.getElementById("typing-animation").textContent += currentProfession.charAt(charIndex);
+      document.getElementById("typing-animation").textContent +=
+        currentProfession.charAt(charIndex);
       charIndex++;
       setTimeout(innerTypeWriter, 100);
     } else {
@@ -53,7 +69,9 @@ function terminalCmdTypeWriter() {
 
   function terminalCmdEraseText() {
     if (document.getElementById("typing-animation").textContent.length > 0) {
-      document.getElementById("typing-animation").textContent = document.getElementById("typing-animation").textContent.slice(0, -1);
+      document.getElementById("typing-animation").textContent = document
+        .getElementById("typing-animation")
+        .textContent.slice(0, -1);
       setTimeout(terminalCmdEraseText, 50);
     } else {
       terminalCmdIndex = (terminalCmdIndex + 1) % terminalCmdProfessions.length;
@@ -65,7 +83,15 @@ function terminalCmdTypeWriter() {
 }
 
 /*-- ABOUT TYPING ANIMATION FOR BAO-g0ddess --*/
-const baoGoddessProfessions = ["DUI Expert", "Cyber Analyst", "Police Officer", "Air Force Veteran", "Snowboarder", "Gardener", "PC Gamer"];
+const baoGoddessProfessions = [
+  "DUI Expert",
+  "Cyber Analyst",
+  "Police Officer",
+  "Air Force Veteran",
+  "Snowboarder",
+  "Gardener",
+  "PC Gamer",
+];
 let baoGoddessIndex = 0;
 
 function baoGoddessTypeWriter() {
@@ -75,7 +101,8 @@ function baoGoddessTypeWriter() {
 
   function innerTypeWriter() {
     if (charIndex < currentProfession.length) {
-      document.getElementById("typing-animation-bao").textContent += currentProfession.charAt(charIndex);
+      document.getElementById("typing-animation-bao").textContent +=
+        currentProfession.charAt(charIndex);
       charIndex++;
       setTimeout(innerTypeWriter, 100);
     } else {
@@ -84,8 +111,12 @@ function baoGoddessTypeWriter() {
   }
 
   function baoGoddessEraseText() {
-    if (document.getElementById("typing-animation-bao").textContent.length > 0) {
-      document.getElementById("typing-animation-bao").textContent = document.getElementById("typing-animation-bao").textContent.slice(0, -1);
+    if (
+      document.getElementById("typing-animation-bao").textContent.length > 0
+    ) {
+      document.getElementById("typing-animation-bao").textContent = document
+        .getElementById("typing-animation-bao")
+        .textContent.slice(0, -1);
       setTimeout(baoGoddessEraseText, 50);
     } else {
       baoGoddessIndex = (baoGoddessIndex + 1) % baoGoddessProfessions.length;
@@ -102,7 +133,12 @@ window.onload = function () {
 };
 
 /*-- ABOUT TYPING ANIMATION FOR Just!ce_s33ker --*/
-const justiceSeekerProfessions = ["Lawyer", "Skier", "Navy Veteran", "Outdoorsman"];
+const justiceSeekerProfessions = [
+  "Lawyer",
+  "Skier",
+  "Navy Veteran",
+  "Outdoorsman",
+];
 let justiceSeekerIndex = 0;
 
 function justiceSeekerTypeWriter() {
@@ -112,7 +148,8 @@ function justiceSeekerTypeWriter() {
 
   function innerTypeWriter() {
     if (charIndex < currentProfession.length) {
-      document.getElementById("typing-animation-Just!ce").textContent += currentProfession.charAt(charIndex);
+      document.getElementById("typing-animation-Just!ce").textContent +=
+        currentProfession.charAt(charIndex);
       charIndex++;
       setTimeout(innerTypeWriter, 100);
     } else {
@@ -121,11 +158,16 @@ function justiceSeekerTypeWriter() {
   }
 
   function justiceSeekerEraseText() {
-    if (document.getElementById("typing-animation-Just!ce").textContent.length > 0) {
-      document.getElementById("typing-animation-Just!ce").textContent = document.getElementById("typing-animation-Just!ce").textContent.slice(0, -1);
+    if (
+      document.getElementById("typing-animation-Just!ce").textContent.length > 0
+    ) {
+      document.getElementById("typing-animation-Just!ce").textContent = document
+        .getElementById("typing-animation-Just!ce")
+        .textContent.slice(0, -1);
       setTimeout(justiceSeekerEraseText, 50);
     } else {
-      justiceSeekerIndex = (justiceSeekerIndex + 1) % justiceSeekerProfessions.length;
+      justiceSeekerIndex =
+        (justiceSeekerIndex + 1) % justiceSeekerProfessions.length;
       setTimeout(justiceSeekerTypeWriter, 500);
     }
   }
@@ -146,54 +188,267 @@ function calculateValue() {
   var weightType = document.getElementById("weightType").value;
 
   var narcoticData = {
-    "Adderall": { "gram": 10.00, "pill": 10.00, "pound": 4540.00, "ounce": null, "kilogram": null, "mL": null },
-    "Alprazolam": { "gram": 10.00, "pill": 10.00, "pound": 4540.00, "ounce": null, "kilogram": null, "mL": null },
-    "Ecstasy": { "gram": 25.00, "pill": 25.00, "pound": 11350.00, "ounce": null, "kilogram": null, "mL": null },
-    "Cocaine (Powder)": { "gram": 125.00, "pound": 22700.00, "ounce": 1200.00, "kilogram": 32000.00, "mL": null },
-    "Cocaine (Crack)": { "gram": 123.00, "pound": 4540.00, "ounce": 1000.00, "kilogram": null, "mL": null },
-    "Fentanyl": { "gram": 155.55, "pound": 70612.70, "ounce": 1500.00, "kilogram": 40000.00, "mL": null },
-    "Heroin (Tan)": { "gram": 100.00, "pound": 45359.20, "ounce": null, "kilogram": null, "mL": null },
-    "Heroin (White)": { "gram": 150.00, "pound": 68038.80, "ounce": null, "kilogram": null, "mL": null },
-    "Heroin (Black Tar)": { "gram": 150.00, "pound": 68038.80, "ounce": null, "kilogram": null, "mL": null },
-    "Hydrocodone 10mg": { "gram": 15.00, "pill": 15.00, "pound": 6810.00, "ounce": null, "kilogram": null, "mL": null },
-    "Hydrocodone 30mg": { "gram": 20.00, "pill": 20.00, "pound": 9080.00, "ounce": null, "kilogram": null, "mL": null },
-    "Hydrocodone 80mg": { "gram": 25.00, "pill": 25.00, "pound": 11350.00, "ounce": null, "kilogram": null, "mL": null },
-    "Ketamine": { "gram": 100.00, "pill": 20.00, "pound": 45400.00, "ounce": null, "kilogram": null, "mL": null },
-    "LSD": { "gram": 5.00, "pill": 10.00, "pound": 2270.00, "ounce": null, "kilogram": null, "mL": null },
-    "Marijuana (Domestic)": { "gram": 4.41, "pound": 2000.00, "ounce": null, "kilogram": null, "mL": null },
-    "Marijuana (Mexican)": { "gram": 2.64, "pound": 1200.00, "ounce": null, "kilogram": null, "mL": null },
-    "Marijuana (Sensimilla)": { "gram": 16.00, "pound": 7256.00, "ounce": null, "kilogram": null, "mL": null },
-    "MDMA": { "gram": 100.00, "pill": 20.00, "pound": 45400.00, "ounce": null, "kilogram": null, "mL": null },
-    "Methamphetamine": { "gram": 330.00, "pound": 1200.00, "ounce": 800.00, "kilogram": null, "mL": null },
-    "Percocet": { "gram": 10.00, "pill": 10.00, "pound": 4540.00, "ounce": null, "kilogram": null, "mL": null },
-    "Psilocybin": { "gram": 9.00, "pound": 4086.00, "ounce": null, "kilogram": null, "mL": null },
-    "Oxycodone 10mg": { "gram": 10.00, "pill": 10.00, "pound": 4540.00, "ounce": null, "kilogram": null, "mL": null },
-    "Oxycodone 30mg": { "gram": 30.00, "pill": 30.00, "pound": 13620.00, "ounce": null, "kilogram": null, "mL": null },
-    "Oxycodone 80mg": { "gram": 50.00, "pill": 50.00, "pound": 22700.00, "ounce": null, "kilogram": null, "mL": null },
-    "Ritalin": { "gram": 3.50, "pill": 3.50, "pound": 1587.00, "ounce": null, "kilogram": null, "mL": null },
-    "Steroids (Liquid) 1": { "ounce": 69.83, "mL": 1117.28 },
-    "Steroids (Liquid) 2": { "kilogram": 2.33 },
-    "Steroids (Powder)": { "ounce": 5.00, "gram": 2270.00 },
-    "Suboxone": { "gram": 10.00, "pill": 10.00, "pound": 4540.00, "ounce": null, "kilogram": null, "mL": null },
-    "Tetrahydrocannabinol (Gummies)": { "gram": 16.00, "pound": 7256.00, "ounce": null, "kilogram": null, "mL": null },
-    "Tetrahydrocannabinol (Liquid)": { "gram": 80.00, "pound": 36320.00, "ounce": null, "kilogram": null, "mL": null },
-    "Tetrahydrocannabinol (Wax)": { "gram": 80.00, "pound": 36320.00, "ounce": null, "kilogram": null, "mL": null },
-    "Viagra": { "gram": 10.00, "pill": 10.00, "pound": 4540.00, "ounce": null, "kilogram": null, "mL": null },
-    "Vicodin": { "gram": 10.00, "pill": 10.00, "pound": 4540.00, "ounce": null, "kilogram": null, "mL": null }
+    Adderall: {
+      gram: 10.0,
+      pill: 10.0,
+      pound: 4540.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Alprazolam: {
+      gram: 10.0,
+      pill: 10.0,
+      pound: 4540.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Ecstasy: {
+      gram: 25.0,
+      pill: 25.0,
+      pound: 11350.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Cocaine (Powder)": {
+      gram: 125.0,
+      pound: 22700.0,
+      ounce: 1200.0,
+      kilogram: 32000.0,
+      mL: null,
+    },
+    "Cocaine (Crack)": {
+      gram: 123.0,
+      pound: 4540.0,
+      ounce: 1000.0,
+      kilogram: null,
+      mL: null,
+    },
+    Fentanyl: {
+      gram: 155.55,
+      pound: 70612.7,
+      ounce: 1500.0,
+      kilogram: 40000.0,
+      mL: null,
+    },
+    "Heroin (Tan)": {
+      gram: 100.0,
+      pound: 45359.2,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Heroin (White)": {
+      gram: 150.0,
+      pound: 68038.8,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Heroin (Black Tar)": {
+      gram: 150.0,
+      pound: 68038.8,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Hydrocodone 10mg": {
+      gram: 15.0,
+      pill: 15.0,
+      pound: 6810.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Hydrocodone 30mg": {
+      gram: 20.0,
+      pill: 20.0,
+      pound: 9080.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Hydrocodone 80mg": {
+      gram: 25.0,
+      pill: 25.0,
+      pound: 11350.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Ketamine: {
+      gram: 100.0,
+      pill: 20.0,
+      pound: 45400.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    LSD: {
+      gram: 5.0,
+      pill: 10.0,
+      pound: 2270.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Marijuana (Domestic)": {
+      gram: 4.41,
+      pound: 2000.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Marijuana (Mexican)": {
+      gram: 2.64,
+      pound: 1200.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Marijuana (Sensimilla)": {
+      gram: 16.0,
+      pound: 7256.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    MDMA: {
+      gram: 100.0,
+      pill: 20.0,
+      pound: 45400.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Methamphetamine: {
+      gram: 330.0,
+      pound: 1200.0,
+      ounce: 800.0,
+      kilogram: null,
+      mL: null,
+    },
+    Percocet: {
+      gram: 10.0,
+      pill: 10.0,
+      pound: 4540.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Psilocybin: {
+      gram: 9.0,
+      pound: 4086.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Oxycodone 10mg": {
+      gram: 10.0,
+      pill: 10.0,
+      pound: 4540.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Oxycodone 30mg": {
+      gram: 30.0,
+      pill: 30.0,
+      pound: 13620.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Oxycodone 80mg": {
+      gram: 50.0,
+      pill: 50.0,
+      pound: 22700.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Ritalin: {
+      gram: 3.5,
+      pill: 3.5,
+      pound: 1587.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Steroids (Liquid) 1": { ounce: 69.83, mL: 1117.28 },
+    "Steroids (Liquid) 2": { kilogram: 2.33 },
+    "Steroids (Powder)": { ounce: 5.0, gram: 2270.0 },
+    Suboxone: {
+      gram: 10.0,
+      pill: 10.0,
+      pound: 4540.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Tetrahydrocannabinol (Gummies)": {
+      gram: 16.0,
+      pound: 7256.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Tetrahydrocannabinol (Liquid)": {
+      gram: 80.0,
+      pound: 36320.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    "Tetrahydrocannabinol (Wax)": {
+      gram: 80.0,
+      pound: 36320.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Viagra: {
+      gram: 10.0,
+      pill: 10.0,
+      pound: 4540.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
+    Vicodin: {
+      gram: 10.0,
+      pill: 10.0,
+      pound: 4540.0,
+      ounce: null,
+      kilogram: null,
+      mL: null,
+    },
   };
 
   var value = narcoticData[narcotic][weightType] * weight;
-  var formattedValue = "$" + value.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  var formattedValue =
+    "$" +
+    value.toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
 
   document.getElementById("result").innerHTML = "Value: " + formattedValue;
 }
 
 /* CLEAR PAGE BUTTON */
-// Function to reload the webpage
-function reloadPage() {
+function reloadPage1() {
   location.reload();
 }
 
-const refreshModalButton = document.getElementById('refreshModalButton');
+function reloadPage2() {
+  location.reload();
+}
 
-refreshModalButton.addEventListener('click', reloadPage);
+const refreshModalButton1 = document.getElementById("refreshModalButton1");
+const refreshModalButton2 = document.getElementById("refreshModalButton2");
+
+refreshModalButton1.addEventListener("click", reloadPage1);
+refreshModalButton2.addEventListener("click", reloadPage2);
